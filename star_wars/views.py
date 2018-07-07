dcd0c82e8e1b2d03d7113409f92ad4021c1d8341
Paul Gelot <pgelot@gmail.com>
2018-07-07 03:56:44 -0400

    all templates rendering

--- star_wars/views.py
+++ star_wars/views.py
@@ -17,31 +17,13 @@ def index():
 
 @app.route('/trilogy', methods = ['GET', 'POST'])
 def trilogy():
+    
     return render_template('trilogy.html')
 
 @app.route('/prequels')
 def prequels():
     return render_template('prequels.html')
 
-# @app.route('/ten', methods = ['GET', 'POST'])
-# def ten():
-#     req = requests.get(url="https://pokeapi.co/api/v2/pokemon-form/")
-
-
-# @app.route('/pokemon/<num>', methods = ['GET','POST'])
-# def pokemon(num):
-#     lst = []
-#     for i in range(1,55):
-#         lst.append(i)
-#     num = i        
-#     req = request.get(url="https://pokeapi.co/api/v2/pokemon-form/")
-#     creature = req.json()
-#     name = creature['name']
-#     return "<h2>Profile for {{name}}</h2>"
-
-# @app.route('/second')
-# def second():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         fightstyle = request.form['fightstyle']
-#         return render_template('/second.html',fightstyle=fightstyle)
+@app.route('/discussion', methods = ['GET', 'POST'])
+def forum():
+    return render_template('discussion.html',)
